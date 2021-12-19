369d0ae16f03d1a2088846a555d37be8a51bb902
이병훈 <bhlee@onns.co.kr>
2021-12-19 15:44:17 +0900

    imageView 확대 축소 기능 추가

--- Besteats/RecommendViewController.swift
+++ Besteats/RecommendViewController.swift
@@ -11,11 +11,20 @@ class RecommendViewController: UIViewController {
 
     @IBOutlet var foodTableImageView: UIImageView!
     
+    var initialImageSize: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(doPinch))
+        self.view.addGestureRecognizer(pinch)
 
         foodTableImageView.image = UIImage(named: "foodsTable")
         
+    }
+    @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        foodTableImageView.transform = foodTableImageView.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+        pinch.scale = 1
     }
     
 }
