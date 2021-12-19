20189974eba1490e89ffa8379c5b74de8fec2255
이병훈 <bhlee@onns.co.kr>
2021-12-19 17:47:30 +0900

    1. scrollview 추가 2. viewForZooming 메소드로 imageView 확대 및 축소 3. 확대, 축소후 scroll (이동 가능하게 구현) 4. 최대, 최소줌 후 한쪽에 붙는 문제 해결

--- Besteats/RecommendViewController.swift
+++ Besteats/RecommendViewController.swift
@@ -10,21 +10,38 @@ import UIKit
 class RecommendViewController: UIViewController {
 
     @IBOutlet var foodTableImageView: UIImageView!
-    
-    var initialImageSize: CGFloat!
+    @IBOutlet var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(doPinch))
-        self.view.addGestureRecognizer(pinch)
-
-        foodTableImageView.image = UIImage(named: "foodsTable")
+        configure()
         
     }
-    @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
-        foodTableImageView.transform = foodTableImageView.transform.scaledBy(x: pinch.scale, y: pinch.scale)
-        pinch.scale = 1
+    
+    func configure() {
+        foodTableImageView.image = UIImage(named: "foodsTable")
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 0.5
+        scrollView.maximumZoomScale = 3.0
     }
     
+}
+
+extension RecommendViewController: UIScrollViewDelegate {
+    // 줌 기능
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.foodTableImageView
+    }
+    
+    // 최대, 최소줌 후 한쪽에 붙는 문제 해결
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale <= 1.0 {
+            scrollView.zoomScale = 1.0
+        }
+
+        if scrollView.zoomScale >= 2.0 {
+            scrollView.zoomScale = 2.0
+        }
+    }
 }
