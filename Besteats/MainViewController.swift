cb621007581c972672861272172be6fabb2f6c38
이병훈 <bhlee@onns.co.kr>
2021-12-24 23:18:04 +0900

    main화면 searchbar 스토리보드 추가

--- Besteats/MainViewController.swift
+++ Besteats/MainViewController.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var foodSearchBar: UISearchBar!
+    @IBOutlet weak var foodLabel1: UILabel!
+    @IBOutlet weak var foodLabel2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
     }
 
-
+    
+    
 }
 
