c29b0573aef2f2fd0f75ba8233f534d4476afc87
이병훈 <bhlee@onns.co.kr>
2021-12-27 22:30:51 +0900

    1. FoodCollectionViewCell 추가 2. UICollectionView 레이아웃 추가 및 ContentView 내 Button 추가 3. 기존 foodLabel 삭제 4. CollectionViewDelegate, dataSource, layout extension에 추가

--- Besteats/MainViewController.swift
+++ Besteats/MainViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var foodSearchBar: UISearchBar!
-    @IBOutlet weak var foodLabel: UILabel!
-    @IBOutlet weak var foodImageView: UIImageView!
+    @IBOutlet weak var foodCollectionView: UICollectionView!
+    
+//    @IBOutlet weak var foodImageView: UIImageView!
     
     var foodList: [String] = ["치킨", "카레", "피자", "대게", "떡볶이", "타코"]
     
@@ -19,7 +20,9 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         
         foodSearchBar.delegate = self
-        foodList.map{ foodLabel.text = $0 }
+        foodCollectionView.delegate = self
+        foodCollectionView.dataSource = self
+//        foodList.map{ foodLabel.text = $0 }
         
     }
 
@@ -28,12 +31,53 @@ class MainViewController: UIViewController {
 }
 
 extension MainViewController: UISearchBarDelegate {
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
         if searchText.count > 1 {
            let filteredFoodArr = foodList.filter { $0 == searchText }
             let filterdFood =  filteredFoodArr.joined(separator: "")
-            foodLabel.text = filterdFood
+            
         }
     }
+}
+
+// Cell layout
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    // 위 아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 40
+    }
+    
+    // 좌우 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 50
+    }
+    
+    // Cell 사이즈
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let size = CGSize(width: 250, height: 100)
+        return size
+    }
+}
+
+// Cell에 대한 delegate
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCell", for: indexPath) as! FoodCollectionViewCell
+        
+        cell.foodButtonLayer.setTitle("\(foodList[indexPath.row])", for: .selected)
+        cell.backgroundColor = .lightGray
+        cell.layer.cornerRadius = 10
+        cell.layer.borderWidth = 1
+
+        return cell
+    }
+    
+    // numberOfItemsInSection: Cell을 몇개 보여줄지
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return foodList.count
+    }
+    
 }
---
+++ FoodCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FoodCollectionViewCell.swift
+//  Besteats
+//
+//  Created by BH on 2021/12/27.
+//
+
+import UIKit
+
+class FoodCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var foodButtonLayer: UIButton!
+    
+    @IBAction func foodButtonAction(_ sender: Any) {
+
+    }
+    
+}
