2b9e52eba32d79c3afe1e77249b6f02fa4a9f8e3
이병훈 <bhlee@onns.co.kr>
2021-12-30 10:56:55 +0900

    1. 아이콘 삭쩨 2. stackView 삭제 및 재배치

--- Besteats/FoodCollectionViewCell.swift
+++ Besteats/FoodCollectionViewCell.swift
@@ -7,20 +7,6 @@
 
 import UIKit
 
-class FoodCollectionViewCell: UICollectionViewCell {
-    
-//    @IBOutlet weak var foodButtonLayer: UIButton!
-    
-    @IBOutlet weak var remindIconsStackView: UIStackView!
-    @IBOutlet weak var foodContentsStackView: UIStackView!
-    
-    
-    
-//    @IBAction func foodButtonAction(_ sender: Any) {
-//
-//    }
-    
-}
 
 extension FoodCollectionViewCell: UICollectionViewDelegate {
     
--- Besteats/MainViewController.swift
+++ Besteats/MainViewController.swift
@@ -11,9 +11,6 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var foodSearchBar: UISearchBar!
     @IBOutlet weak var foodCollectionView: UICollectionView!
-    
-    
-    
 //    @IBOutlet weak var foodImageView: UIImageView!
     
     var foodList: [String] = ["치킨", "카레", "피자", "대게", "떡볶이", "타코"]
@@ -27,9 +24,13 @@ class MainViewController: UIViewController {
 //        foodList.map{ foodLabel.text = $0 }
         
     }
+    
+    class FoodCollectionViewCell: UICollectionViewCell {
+        
+        @IBOutlet weak var title: UILabel!
+        
+    }
 
-    
-    
 }
 
 extension MainViewController: UISearchBarDelegate {
@@ -67,9 +68,8 @@ extension MainViewController: UICollectionViewDelegateFlowLayout {
 // Cell에 대한 delegate
 extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCell", for: indexPath) as! FoodCollectionViewCell
+         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCell", for: indexPath) as? FoodCollectionViewCell else { return UICollectionViewCell() }
         
-//        cell.foodButtonLayer.setTitle("\(foodList[indexPath.row])", for: .selected)
         cell.backgroundColor = .lightGray
         cell.layer.cornerRadius = 10
         cell.layer.borderWidth = 1
