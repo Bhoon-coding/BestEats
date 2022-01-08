2ab2446b7abdc9d39021c175821b5b00d02227fa
이병훈 <bhlee@onns.co.kr>
2022-01-08 19:25:35 +0900

    BottomSheet 열리는 애니메이션

--- Besteats/BottomSheetViewController.swift
+++ Besteats/BottomSheetViewController.swift
@@ -15,14 +15,34 @@ class BottomSheetViewController: UIViewController {
         return view
     }()
     
+    private let bottomSheetView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private var bottomSheetViewTopConstraint: NSLayoutConstraint!
+    var defaultHeight: CGFloat = 250
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupUI()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            showBottomSheet()
+        }
+    
     private func setupUI() {
         view.addSubview(dimmedView)
+        view.addSubview(bottomSheetView)
+        
+        dimmedView.alpha = 0.0
         
         setupLayout()
     }
@@ -36,5 +56,28 @@ class BottomSheetViewController: UIViewController {
             dimmedView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
 
+        bottomSheetView.translatesAutoresizingMaskIntoConstraints = false
+                let topConstant = view.safeAreaInsets.bottom + view.safeAreaLayoutGuide.layoutFrame.height
+                bottomSheetViewTopConstraint = bottomSheetView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstant)
+                NSLayoutConstraint.activate([
+                    bottomSheetView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                    bottomSheetView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                    bottomSheetView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                    bottomSheetViewTopConstraint
+                ])
+    }
+    
+    private func showBottomSheet() {
+        let safeAreaHeight: CGFloat = view.safeAreaLayoutGuide.layoutFrame.height
+        let bottomPadding: CGFloat = view.safeAreaInsets.bottom
+        
+        bottomSheetViewTopConstraint.constant = (safeAreaHeight + bottomPadding) - defaultHeight
+        
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn, animations: {
+                    // 4 - 1
+            self.dimmedView.alpha = 0.7
+                    // 4 - 2
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
 }
