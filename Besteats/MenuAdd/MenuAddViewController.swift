d33687a6a5163654a2cd2dae8c05354084f09451
이병훈 <bhlee@onns.co.kr>
2022-03-15 23:05:10 +0900

    폴더 이름 수정 및 정리, MenuAdd 폴더 추가

---
+++ Besteats/MenuAdd/MenuAddViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuAddViewController.swift
+//  Besteats
+//
+//  Created by BH on 2022/03/15.
+//
+
+import UIKit
+
+import SnapKit
+
+class MenuAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+}
