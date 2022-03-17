89c7eed0987e26f44abf5a6d518a7c159a8f1a17
이병훈 <bhlee@onns.co.kr>
2022-03-17 23:51:09 +0900

    menuAddVC 진입, 레이아웃, '메뉴추가' 버튼 터치시 창 닫기 구현

--- Besteats/FoodDetail/ViewController/FoodDetailViewController.swift
+++ Besteats/FoodDetail/ViewController/FoodDetailViewController.swift
@@ -9,52 +9,6 @@ import UIKit
 
 import SnapKit
 
-class FoodDetailTableViewCell: UITableViewCell {
-    static let identifier = "FoodDetailTableViewCell"
-    
-    lazy var menuLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont(name: "BM JUA_OTF", size: 30)
-        label.text = "메뉴명"
-        return label
-    }()
-    
-    lazy var oneLinerLabel: UILabel = {
-        let label = UILabel()
-        label.text = "∙ 소스에 찍먹"
-        label.font = UIFont(name: "BM JUA_OTF", size: 20)
-        return label
-    }()
-    
-    lazy var favoriteButton: UIButton = {
-        let button = UIButton()
-        return button
-    }()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setUpCell()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setUpCell() {
-        contentView.addSubview(menuLabel)
-        contentView.addSubview(oneLinerLabel)
-        
-        menuLabel.snp.makeConstraints {
-            $0.leading.top.equalToSuperview().inset(30)
-        }
-        
-        oneLinerLabel.snp.makeConstraints {
-            $0.leading.equalTo(menuLabel)
-            $0.top.equalTo(menuLabel.snp.bottom).offset(30)
-        }
-    }
-}
-
 class FoodDetailViewController: UIViewController {
     
     // MARK: Properties
@@ -195,8 +149,8 @@ class FoodDetailViewController: UIViewController {
     
     @objc func addTapped() {
         
-        let restaurantAddVC = RestaurantAddViewController()
-        present(restaurantAddVC, animated: true, completion: nil)
+        let menuAddVC = MenuAddViewController()
+        present(menuAddVC, animated: true, completion: nil)
     }
     
     @objc func tappedLikeButton() {
