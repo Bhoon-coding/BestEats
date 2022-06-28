b81e3e74b5771802840c795c75a0cc64d4d9a1cb
bhoon-coding <bhooncoding@gmail.com>
2022-06-29 00:12:32 +0900

    [feature] #52 - add pagination layout

--- Besteats/Recommendation/FoodImageViewController.swift
+++ Besteats/Recommendation/FoodImageViewController.swift
@@ -21,18 +21,18 @@ final class FoodImageViewController: UIViewController {
     // MARK: - Properties
     
     var imageArr: [String] = []
-    
     let foodType: String
     
     init(foodType: String) {
         self.foodType = foodType
-        
         super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - UIProperties
     
     private lazy var closeButton: UIButton = {
         let button = UIButton()
@@ -50,6 +50,13 @@ final class FoodImageViewController: UIViewController {
         return imageView
     }()
     
+    private lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = imageArr.count
+        pageControl.backgroundColor = #colorLiteral(red: 0.2279180713, green: 0.3073120006, blue: 1, alpha: 1)
+        return pageControl
+    }()
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
@@ -65,6 +72,9 @@ final class FoodImageViewController: UIViewController {
                 }
                 foodImageURLs.forEach {
                     self.imageArr.append($0)
+                }
+                DispatchQueue.main.async {
+                    self.pageControl.numberOfPages = self.imageArr.count
                 }
                 
             case .failure:
@@ -94,7 +104,8 @@ extension FoodImageViewController {
         view.backgroundColor = .white
         
         [closeButton,
-         foodImageView
+         foodImageView,
+         pageControl
         ]
             .forEach { view.addSubview($0) }
         
@@ -106,6 +117,13 @@ extension FoodImageViewController {
         foodImageView.snp.makeConstraints {
             $0.leading.top.trailing.equalToSuperview()
             $0.bottom.equalToSuperview().inset(80)
+        }
+        
+        pageControl.snp.makeConstraints {
+            $0.top.equalTo(foodImageView.snp.bottom).inset(40)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(240)
+            $0.height.equalTo(60)
         }
         
     }
