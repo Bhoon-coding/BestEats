24104c5452cf7d00454565e4b2a2b6c1a3675581
bhoon-coding <bhooncoding@gmail.com>
2022-08-08 21:56:08 +0900

    [feature] #54 - NetworkRequesterType, NetworkRequester, 공통으로 사용할 dataTask 메서드 구현

--- Besteats/Network/NetworkRequester/NetworkRequester.swift
+++ Besteats/Network/NetworkRequester/NetworkRequester.swift
@@ -30,6 +30,59 @@ enum NetworkError: LocalizedError {
     }
 }
 
+protocol NetworkRequesterType {
+    
+    func request(to endPoint: EndPointType, completion: @escaping (Result<Data, NetworkError>) -> Void)
+    
+    func request(to urlString: String, completion: @escaping (Result<Data, NetworkError>) -> Void) -> URLSessionDataTask?
+    
+}
 
-
-
+struct NetworkRequester: NetworkRequesterType {
+    
+    private let session: URLSession = .shared
+    
+    func request(
+        to endPoint: EndPointType,
+        completion: @escaping (Result<Data, NetworkError>) -> Void
+    ) {
+        guard let urlRequest = endPoint.asURLRequest() else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        dataTask(request: urlRequest, completion: completion).resume()
+    }
+    
+    func request(to urlString: String,
+                 completion: @escaping (Result<Data, NetworkError>) -> Void
+    ) -> URLSessionDataTask? {
+        <#code#>
+    }
+    
+    private func dataTask(
+        request: URLRequest,
+        completion: @escaping (Result<Data, NetworkError>) -> Void
+    ) -> URLSessionDataTask {
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(.requestFail(error)))
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            guard 200..<300 ~= httpResponse.statusCode else {
+                completion(.failure(.failedResponse(statusCode: httpResponse.statusCode)))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.emptyData))
+                return
+            }
+            completion(.success(data))
+        }
+        return task
+    }
+    
+}
