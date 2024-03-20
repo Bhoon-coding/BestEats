9670c528223e5b503be86e94af378d1631eb48ef
bhoon-coding <bhooncoding@gmail.com>
2024-03-20 09:55:05 +0900

    github 연동 테스트

--- Besteats/Model/Restaurants.swift
+++ Besteats/Model/Restaurants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Restaurant: Codable, Equatable {
-    var restaurantName: String // test
+    var restaurantName: String
     var type: String
     var likeMenus: [Menu]
     var curiousMenus: [Menu]
